8513350cfcac134af093b45b1b5eb2a2057072ca
Fer96carvalho <f.luiz1996@gmail.com>
2023-12-11 21:20:21 -0300

    commit

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Origin,X-Requested-With,Accept');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 
 const { connectDatabase } = require("./database");
